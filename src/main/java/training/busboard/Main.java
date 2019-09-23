8ad1125e87c0e9c4c86c7b52caef22a99d97c0c6
Daniel Field <dannyf506@googlemail.com>
2019-09-23 16:26:15 +0100

    Added object

--- src/main/java/training/busboard/Main.java
+++ src/main/java/training/busboard/Main.java
@@ -3,7 +3,9 @@ package training.busboard;
 import org.glassfish.jersey.jackson.JacksonFeature;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.MediaType;
+import java.util.List;
 
 
 public class Main {
@@ -11,9 +13,9 @@ public class Main {
 
         Client client = ClientBuilder.newBuilder().register(JacksonFeature.class).build();
 
-        String name = client.target("https://api.tfl.gov.uk/StopPoint/490008660N/Arrivals").request(MediaType.TEXT_PLAIN).get(String.class);
+        List<BusInfo> response = client.target("https://api.tfl.gov.uk/StopPoint/490008660N/Arrivals").request(MediaType.APPLICATION_JSON_TYPE).get(new GenericType<List<BusInfo>>() {});
 
-     System.out.println(name);
+     //System.out.println(name.);
 
     }
 }	
