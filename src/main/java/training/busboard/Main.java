13ad8bd87d13adcd7d4fad3efc18483f15ec40b1
Daniel Field <dannyf506@googlemail.com>
2019-09-23 15:18:59 +0100

    reset back to this if zee's changes break everything

--- src/main/java/training/busboard/Main.java
+++ src/main/java/training/busboard/Main.java
@@ -1,8 +1,6 @@
 package training.busboard;
 
-import org.glassfish.jersey.client.ClientRequest;
 import org.glassfish.jersey.jackson.JacksonFeature;
-
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.core.MediaType;
@@ -14,6 +12,7 @@ public class Main {
         Client client = ClientBuilder.newBuilder().register(JacksonFeature.class).build();
 
         String name = client.target("https://api.tfl.gov.uk/StopPoint/490008660N/Arrivals").request(MediaType.TEXT_PLAIN).get(String.class);
-     System.out.println(name);
+
+     //System.out.println(name);
     }
 }	
