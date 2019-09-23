c45fb6af35a7b3decbc0add67d913c4264db1df8
Daniel Field <dannyf506@googlemail.com>
2019-09-23 14:47:26 +0100

    Added client.target

--- src/main/java/training/busboard/Main.java
+++ src/main/java/training/busboard/Main.java
@@ -1,14 +1,19 @@
 package training.busboard;
 
+import org.glassfish.jersey.client.ClientRequest;
 import org.glassfish.jersey.jackson.JacksonFeature;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.MediaType;
+
 
 public class Main {
     public static void main(String args[]) {
+
         Client client = ClientBuilder.newBuilder().register(JacksonFeature.class).build();
 
-
+        String name = client.target("https://api.tfl.gov.uk/StopPoint/490008660N/Arrivals").request(MediaType.TEXT_PLAIN).get(String.class);
+     System.out.println(name);
     }
 }	
