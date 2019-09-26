3ceafb123302622489794ee6d2c41f5307d34c23
Zee <zee.saydullaev@aexp.com>
2019-09-26 16:05:01 +0100

    added working NaptanID

--- src/main/java/training/busboard/StopPoint.java
+++ src/main/java/training/busboard/StopPoint.java
@@ -1,12 +1,10 @@
 package training.busboard;
 import org.codehaus.jackson.annotate.JsonIgnoreProperties;
-
 import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 
 public class StopPoint {
-     //public List <Codes> stopPoints;
     public List <NaptanIDs> stopPoints;
     
 }
